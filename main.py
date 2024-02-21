565257b2aa03b1fcdecff6b5f954603d6b5bed12
Zain Ul Abdin Siyal <113477680+Zain1443@users.noreply.github.com>
2024-02-21 13:42:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+ 
+# In[4]:
+
+
 from flask import Flask, jsonify
+import os
 import psycopg2
 
+# Create flask app variable
 app = Flask(__name__)
 
-# PostGIS database connection details
-dbname = 'lab1'
+# Create variables with sign-in details
+database = 'lab1'
 user = 'postgres'
 password = 'Hyderabad43%'
-host = '34.122.229.143'  # Update to your SQL Database external IP
+host = '34.122.229.143'
 port = '5432'
 
-//@app.route('/get_polygon', methods=['GET'])
+# Create a route to return polygon in GeoJSON format
 @app.route('/')
-def get_polygon():
-    # Connect to the PostGIS database
-    connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-    cursor = connection.cursor()
+def get_gj():
+    # Connect to DB
+    conn = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
+    # Create cursor
+    cursor = conn.cursor()
 
-    # Replace 'your_table' with the actual table name
-    table_name = 'Zain_Table'
+    # Creates GeoJSON string based on polygon stored in PostgreSQL DB
+    sql_query = """
+    SELECT 
+            json_build_object(
+                'type', 'FeatureCollection',
+                'features', json_agg(
+                    json_build_object(
+                        'type', 'Feature',
+                        'geometry', ST_AsGeoJSON(ST_SetSRID(geom, 4326))::json,
+                        'properties', json_build_object()
+                    )
+                ),
+                'crs', 
+                json_build_object(
+                    'type', 'name',
+                    'properties', 
+                    json_build_object(
+                        'name', 'epsg:4326'
+                    )
+                )
+            ) AS geojson
+        FROM lab0.public.lab1
+    """
 
-    # Execute SQL query to retrieve the polygon as GeoJSON
-    sql_query = f"SELECT ST_AsGeoJSON(geom) FROM {table_name};"
+    # Gather polygon geojson from above query
     cursor.execute(sql_query)
+    gj = cursor.fetchone()[0]
 
-    # Fetch the result
-    geojson = cursor.fetchone()[0]
-
-    # Close database connection
     cursor.close()
-    connection.close()
+    conn.close()
 
-    return jsonify({"type": "FeatureCollection", "features": [{"type": "Feature", "geometry": geojson}]})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    return gj
+    
+# Run Flask app
+if __name__ == "__main__":
+    app.run(
+        debug = False,
+        host = '0.0.0.0',
+        port = int(os.environ.get("PORT", 5100))
+    )
